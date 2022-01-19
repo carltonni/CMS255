bcca27aa61e49992d76f06aed6b26fc8153f9d8e
carltonni <98037716+carltonni@users.noreply.github.com>
2022-01-19 10:08:55 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*******************************************************************
+* Java Program Name
+********************************************************************
+* Carlton Nivens
+* 1/19/2022
+* C90
+********************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("<Carlton Nivens>");
+		System.out.println("<Hello>");
+		System.out.println("<Lab1>");
+		System.out.println("<1/19/2022>");
+		System.out.println("<C90>");
+		System.out.println();
+	}
+}
